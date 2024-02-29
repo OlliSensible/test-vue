0f1977ea9c3fce27d8527468eed1fa19507f796c
OlhaSenchyk <olhasenchyk@gmail.com>
2024-02-29 11:16:56 +0100

    TT-1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ButtonFirst>Click me</ButtonFirst>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import ButtonFirst from './components/ButtonFirst.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ButtonFirst
   }
 }
 </script>
---
+++ src/components/ButtonFirst.vue
@@ -0,0 +1,18 @@
+<template>
+    <button>
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'ButtonFirst',
+        props: {
+            type: String
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
